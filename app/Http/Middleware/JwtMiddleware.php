12ce0b2674b6dd1a2be393781225f6b9459176ea
Manoj <manoj.m@inapp.com>
2022-04-28 11:51:02 +0530

    middle ware change, angular code update

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -23,9 +23,9 @@ class JwtMiddleware extends BaseMiddleware
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-                return response()->json(['status' => 'Token is Invalid']);
+                return response()->json(['success' => false,'message' => 'Token is Invalid'], 401);
             }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['status' => 'Token is Expired']);
+                return response()->json(['success' => false,'message' => 'Token is Expired'], 401);
             }else{
                 return response()->json(['status' => 'Authorization Token not found']);
             }
