58c48425bdbcfecb17eb39f61e00547f5010cfed
Manoj <manoj.m@inapp.com>
2022-06-21 00:16:30 +0530

    angular updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,6 +12,6 @@
 
 <body>
   <app-root></app-root>
-<script src="runtime.fffa370b34171da15c04.js" defer></script><script src="polyfills.6e04d3479494a631b187.js" defer></script><script src="scripts.3604a9e8f2c99e6de855.js" defer></script><script src="main.c5b4f7bda30782d6dd44.js" defer></script>
+<script src="runtime.fffa370b34171da15c04.js" defer></script><script src="polyfills.6e04d3479494a631b187.js" defer></script><script src="scripts.3604a9e8f2c99e6de855.js" defer></script><script src="main.652df6628caa8e8eab4f.js" defer></script>
 
 </body></html>
