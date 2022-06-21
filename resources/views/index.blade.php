2b96c677cf1c0a5b1f82e32892c0a60eb4bc203c
Manoj <manoj.m@inapp.com>
2022-06-21 13:06:19 +0530

    routing correction

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,6 +12,6 @@
 
 <body>
   <app-root></app-root>
-<script src="runtime.fffa370b34171da15c04.js" defer></script><script src="polyfills.6e04d3479494a631b187.js" defer></script><script src="scripts.3604a9e8f2c99e6de855.js" defer></script><script src="main.652df6628caa8e8eab4f.js" defer></script>
+<script src="runtime.fffa370b34171da15c04.js" defer></script><script src="polyfills.6e04d3479494a631b187.js" defer></script><script src="scripts.3604a9e8f2c99e6de855.js" defer></script><script src="main.b603bd7247f5e7273e03.js" defer></script>
 
 </body></html>
